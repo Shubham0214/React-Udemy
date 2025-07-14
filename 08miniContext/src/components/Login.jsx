c05a73b666a573d63f6b0086ba9d24a0f91b65b0
Shubham0214 <shubhedu0112@gmail.com>
2025-07-14 15:17:44 +0530

    Complete theme switcher with useState and useEffect

---
+++ 08miniContext/src/components/Login.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useState } from "react";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    //Prevent the page from refreshing
+
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <h2>Login</h2>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Username"
+      />
+    </div>
+  );
+}
+
+export default Login;
